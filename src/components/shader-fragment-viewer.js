3a88e599067f4888c0010fe72c118496a576a6d2
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2022-10-14 18:31:11 -0300

    created a basic three js setup with the custom shader

--- src/components/shader-fragment-viewer.js
+++ src/components/shader-fragment-viewer.js
@@ -1,3 +1,5 @@
+import * as THREE from "three";
+
 class ShaderViewer extends HTMLElement {
   constructor() {
     super();
@@ -10,6 +12,11 @@ class ShaderViewer extends HTMLElement {
     return /*css*/ `
       :host {
         display: block;
+      }
+
+      canvas {
+        width: 100vw;
+        height: 100vh;
       }
     `;
   }
@@ -25,6 +32,74 @@ class ShaderViewer extends HTMLElement {
 
   init() {
     console.log("init");
+
+    /* TODO para el lenny del futuro
+      - armar bien la parte de three y persistir
+      - poder persistir el shader
+      - poder cambiar el shader sin tener que recargar la pagina
+      - pensar mas todos 💩
+     
+    */
+
+    const canvas = this.shadowRoot.querySelector(".pepitos");
+    const renderer = new THREE.WebGLRenderer({ canvas });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.z = 2;
+
+    const scene = new THREE.Scene();
+
+    const geometry = new THREE.PlaneGeometry(
+      window.innerWidth,
+      window.innerHeight
+    );
+    const uresolutionForPlane = /*glsl*/ `
+        void main() {
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `;
+
+    // const geometry = new THREE.BoxGeometry(1, 1, 1);
+    // const uresolutionForBoxes = /*glsl*/`
+    //   varying vec2 vUv;
+    //   void main()	{
+    //     vUv = uv;
+    //     gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+    //   }
+    // `;
+
+    const uniforms = {
+      u_resolution: {
+        type: "vec2",
+        value: new THREE.Vector2(window.innerWidth, window.innerHeight),
+      },
+      u_time: { type: "float", value: 1.0 },
+      u_mouse: { type: "vec2", value: new THREE.Vector2() },
+    };
+
+    const material = new THREE.ShaderMaterial({
+      vertexShader: uresolutionForPlane,
+      fragmentShader: this.fragmentShader,
+      uniforms: uniforms,
+    });
+
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    function render() {
+      uniforms.u_time.value += 0.05;
+
+      renderer.render(scene, camera);
+      requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
   }
 
   onFileDropped(e) {
@@ -43,7 +118,7 @@ class ShaderViewer extends HTMLElement {
     this.shadowRoot.innerHTML = /*html*/ `
       <style>${ShaderViewer.styles}</style>
       <div class="shader-viewer">
-        <h1>Shader Fragment Viewer</h1>
+        <canvas class="pepitos"></canvas>
 
         <drop-area></drop-area>
       </div>

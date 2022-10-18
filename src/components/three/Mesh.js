0fc1f71d67aaf8cfe201b00c713f08f82cc60f98
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2022-10-18 13:51:16 -0300

    created basic mesh class with its geometry

---
+++ src/components/three/Mesh.js
@@ -0,0 +1,50 @@
+import * as THREE from "three";
+import App from "./App";
+
+import vertexShader from "./shaders/plane.vertex.glsl?raw";
+
+export default class Mesh {
+  constructor() {
+    this.app = new App();
+    this.sizes = this.app.sizes;
+    this.scene = this.app.scene;
+    this.camera = this.app.camera;
+    this.renderer = this.app.renderer;
+    this.fragmentShader = this.app.fragmentShader;
+
+    this.setPlane();
+  }
+
+  setPlane() {
+    this.geometry = new THREE.PlaneGeometry(
+      this.sizes.width.toFixed(2),
+      this.sizes.height.toFixed(2)
+    );
+
+    this.vertexShader = vertexShader;
+    this.uniforms = {
+      u_resolution: {
+        type: "vec2",
+        value: new THREE.Vector2(
+          this.sizes.width.toFixed(2),
+          this.sizes.height.toFixed(2)
+        ),
+      },
+      u_time: { type: "float", value: 1.0 },
+      u_mouse: { type: "vec2", value: new THREE.Vector2() },
+    };
+
+    this.material = new THREE.ShaderMaterial({
+      vertexShader: this.vertexShader,
+      fragmentShader: this.fragmentShader,
+      uniforms: this.uniforms,
+    });
+
+    this.setMesh();
+  }
+
+  setMesh() {
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.mesh);
+  }
+}
